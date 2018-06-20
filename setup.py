701adb90174d921e7e9de9f49bcce3723f306eb0
Arden Rasmussen <ardenisthebest@gmail.com>
2018-06-19 18:34:47 -0700

    Changed to markdown

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def package_files(directory):
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
